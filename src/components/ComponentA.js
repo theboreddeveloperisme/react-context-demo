94af16c3a91eb123e1a689364ce11b8738162b28
Jan Kosinski <jan.k.kosinski@gmail.com>
2025-04-11 13:40:49 +0200

    ✨ Add Components

---
+++ src/components/ComponentA.js
@@ -0,0 +1,11 @@
+const ComponentA = () => {
+    return (
+      <div>
+        <h3>Component A</h3>
+        <p>This will update the theme context.</p>
+      </div>
+    );
+  };
+  
+  export default ComponentA;
+  
---
+++ src/components/ComponentB.js
@@ -0,0 +1,11 @@
+const ComponentB = () => {
+    return (
+      <div>
+        <h3>Component B</h3>
+        <p>This will display the current theme context.</p>
+      </div>
+    );
+  };
+  
+  export default ComponentB;
+  
